cc487e3848a63a4ed9d503dbe9490af047486ac3
ojoifeanyi <37763914+ojoifeanyi@users.noreply.github.com>
2018-03-31 04:50:03 -0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<!-- saved from url=(0027)http://apply.felixihub.com/ -->
+<html lang="en"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+
+	
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<title>Ifeanyi Ojo - FavCode Test</title>
+
+	<meta name="description" content="African development through partnership">
+
+
+	<!-- Open Graph data -->
+	<meta property="og:title" content="Submission for Felix - Favcode ">
+	<meta property="og:type" content="article">
+	<meta property="og:image" content="Felix%20-%20Favcoder_files/art.png">
+	<meta property="og:description" content="African development through partnership">
+
+	<link href="./Felix - FavCode Test_files/css.css" rel="stylesheet">
+	<link rel="stylesheet" href="ifeanyi_ojo.css">
+	<link rel="stylesheet" href="./Felix - FavCode Test_files/style.css">
+</head>
+<body cz-shortcut-listen="true">
+	<div class="container">
+		<header>
+			<!-- Create a header -->
+		</header>
+
+		<section class="main">
+			<div class="left" align="justify">
+				<blockquote >
+                <h1>Hi, I'm  Ifeanyi Ojo <span class="for">and I am a </span> Favcoder</h1>
+				<p style="color:#424949;">My mission for Favcode is to:  Develop and deploy enterprise software products, hence using Information system to solve todays problems.</p>
+                
+                <h3>This is why you should choose me</h3>
+                <p style="color:#424949;">I may not be the best, however 3 things makes me different viz:</p>
+                <p style="color:#424949;"><b >PASSION</b> - This is something that keeps me going against all odds. </p>
+                 <p style="color:#424949;"><b >ABILITY TO LEARN NEW STUFF AND AT A FAST PACE</b> - Almost every year, new technology comes into light and it is always required that one most keep updating oneself on the use of these new techs. I can learn new things at a fast pace once i am commited to it .  </p>
+                 <p style="color:#424949;"><b >TEAM WORK</b> - Been able to work independenty and as a Team can bring about better and faster results, these i am capable</p>
+
+                 <p style="color:#424949;">When i am given the right skills, opportunity and a platform, i will be able to achieve better and faster results.</p>
+
+                <p class="meta">Here is my <a href="https://www.linkedin.com/in/ifeanyiojo" style="text-decoration: none" target="_blank">LinkedIn Page</a>.</p>
+
+				</blockquote>
+			</div>
+
+			<div class="right">
+				<blockquote><img src="ifeanyi.jpg" alt=""></blockquote>
+			</div>
+		</section>
+
+		<footer class="foot">
+			<p>March 28, 2018. Submission for Felix - Favcode Partnership</p>
+			<p>Made by <a href="https://facebook.com/ifeanyi.ojo.12" style="text-decoration: none" target="_blank">Ifeanyi Ojo.</a></p>
+		</footer>
+	</div>
+
+
+</body></html>
