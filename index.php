1bf1bfbbd763ba0f32591e717e97c4c600c2ded6
ojoifeanyi <37763914+ojoifeanyi@users.noreply.github.com>
2018-04-01 15:01:22 -0700

    Add files via upload

--- index.php
+++ index.php
@@ -21,51 +21,50 @@
 </head>
 <body cz-shortcut-listen="true">
 	<div class="container">
-		<header>
-			<!-- Create a header -->
-		</header>
 
-		<section class="main">
-			<div class="left" align="justify">
-				<blockquote >
-                <h1 style="background-color:#FBFCFC; font-size: 40px;text-align: center;">Hi, I'm  Ifeanyi Ojo <span class="for">and I am a </span> Favcoder</h1>
-				<p style="color:#424949;">My mission for Favcode is to:  Develop and deploy enterprise software products, hence using Information system to solve todays problems.</p>
+		<div class="myheader">
+			<p class="left">I'm Ifeanyi Ojo and i am a FavCoder</p>
+				<p class="right">
+					<iframe width="560" height="315" src="https://www.youtube.com/embed/Nuv_ncYajWg?rel=0" frameborder="0"  encrypted-media" allowfullscreen></iframe>
+			</p>
+		</div>
 
-				<iframe width="560" height="315" src="https://www.youtube.com/embed/Nuv_ncYajWg?rel=0" frameborder="0"  encrypted-media" allowfullscreen></iframe>
-                
-                <h3>This is why you should choose me</h3>
-                <p style="color:#424949;">I may not be the best, however 3 things makes me different viz:</p>
-                <p style="color:#424949;"><b >Passion</b> - This is something that keeps me going against all odds. </p>
-                 <p style="color:#424949;"><b >Ability To Learn New Stuff And At A Fast Pace</b> - Almost every year, new technology comes into light and it is always required that one most keep updating oneself on the use of these new techs. I can learn new things at a fast pace once i am commited to it .  </p>
-                 <p style="color:#424949;"><b >Team Work</b> - Been able to work independenty and as a Team can bring about better and faster results, these i am capable</p>
+		<div class="mission" style="color:#424949; background-color:#ECF0F1; font-size:23px;text-align:center;height:auto;padding: 80px;">
+			<blockquote>
+				<h1>MISSION</h1>
+				<p>My mission for Favcode is to:  Develop and deploy enterprise software products, hence using Information system to solve todays problems.</p>
 
-                 <p style="color:#424949;">When i am given the right skills, opportunity and a platform, i will be able to achieve better and faster results.</p>
+			</blockquote>
+				
+		</div>
+		<div class="me" style="color:#979A9A; background-color:#212627  ; font-size:19px;text-align:justify;height:auto;padding: 80px;">
+			<blockquote>
+				 <h1 style="text-align: center;">This is why you should choose me</h1><br>
+                <p >I may not be the best, however 3 things makes me different viz:</p><br>
+                <p ><b >Passion</b> - This is something that keeps me going against all odds. </p><br>
+                 <p ><b >Ability To Learn New Stuff And At A Fast Pace</b> - Almost every year, new technology comes into light and it is always required that one most keep updating oneself on the use of these new techs. I can learn new things at a fast pace once i am commited to it .  </p><br>
+                 <p ><b >Team Work</b> - Been able to work independenty and as a Team can bring about better and faster results, these i am capable</p><br>
 
-                 <hr>
+                 <p >When i am given the right skills, opportunity and a platform, i will be able to achieve better and faster results.</p><br>
 
-                <p class="meta">Here is my <a href="https://www.linkedin.com/in/ifeanyiojo" style="text-decoration: none" target="_blank">LinkedIn Page</a>.</p>
+            
+			</blockquote>
+				
+		</div>
+		<div class="imgg" style="color:#424949;font-size:23px;text-align:center;height:auto;padding: 50px;">
+			<blockquote>
+				<img src="ifeanyi.jpg" alt="">
 
-				</blockquote>
-			</div>
-
-			<div class="right">
-				<blockquote>
-					<div style="padding-top: 80px;padding-left: 80px;" width:50%;">
-					<img src="ifeanyi.jpg" alt="">
-				</div>
-
-						<div align="left" style="padding-top: 10px;padding-left: 100px;" width:50%;">
-						<p style="color:#424949;text-align: left;font-weight: bold;">PROFILE</p>
-							<p style="color:#424949;text-align: left;"><strong>Name:</strong> Ifeanyichukwu Ike Ojo</p>
+				<br><p style="color:#424949;text-align: center;font-weight: bold;">PROFILE</p><br>
+							<p style="color:#424949;"><strong>Name:</strong> Ifeanyichukwu Ike Ojo</p>
 							<p style="color:#424949;"><strong>Location:</strong> Port Harcourt, Nigeria.</p>
 							<p style="color:#424949;"><strong>Passion:</strong> Programming, Information System, Tech Enthusiast</p>
 							<p style="color:#424949;"><strong>Skills:</strong> Chat bot Developer, Back-end Developer</p>
 							<p style="color:#424949;"><strong>Status:</strong> Student at <a href="https://favcode54.org/" style="text-decoration:none;">#FavCode54</a></p>
-						</div>
-
-				</blockquote>
-			</div>
-		</section>
+							 <p class="meta">Here is my <a href="https://www.linkedin.com/in/ifeanyiojo" style="text-decoration: none" target="_blank">LinkedIn Page</a>.</p>
+			</blockquote>
+				
+		</div>
 
 		<footer class="foot">
 			<p>March 28, 2018. Submission for Felix - Favcode Partnership</p>
